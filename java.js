2a4d514cd16248056273d4dbc6adeb1ec94f2b85
Warren Insell <insell.warren@gmail.com>
2022-03-31 15:37:23 -0700

    Perfect rock, paper, scissors else if statement

--- java.js
+++ java.js
@@ -2,33 +2,30 @@ function computerPlay(random) {
   return random[Math.floor(Math.random()*random.length)];
 }
 
-let random = ['Rock', 'Paper', 'Scissors'];
-console.log(computerPlay(random));
-
+let random = ['rock', 'paper', 'scissors'];
+console.log(computerPlay(random))
 
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
-    accouncement = 'You Win! Rock beats Scissors';
-  } else if (playerSelection == 'Rock' && computerSelection == 'Paper') {
-     announcement = 'You Lose! Paper beats Rock';
-} else if (playerSelection == 'Rock' && computerSelection == 'Rock') {
-    announcement = 'Draw!';
-} else if (playerSelection == 'Paper' && computerSelection == 'Scissors') {
-    announcement = 'You Lose! Scissors beats paper';
-} else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
-    annoucement = 'You Win! Paper beats rock';
-} else if (playerSelection == 'Paper' && computerSelection == 'Paper') {
-    announcement = 'Draw!';
-} else if (playerSelection == 'Scissors' && computerSelection == 'Scissors') {
-    announcement = 'Draw!';
-} else if (playerSelection == 'Scissors' && computerSelection == 'Rock') {
-    announcement = 'You Lose! Rock beats Scissors';
-} else (playerSelection != 'Rock' || 'Paper' || 'Scissors') {
-    announcement = 'You have made an invalid choice'
+  if (playerSelection === 'rock' && computerSelection == 'scissors') {
+    return 'You Win! Rock beats Scissors';
+  } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+     return 'You Lose! Paper beats Rock';
+} else if (playerSelection === 'rock' && computerSelection === 'rock') {
+    return 'Draw!';
+} else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+    return 'You Lose! Scissors beats paper';
+} else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    return 'You Win! Paper beats rock';
+} else if (playerSelection === 'paper' && computerSelection === 'paper') {
+    return'Draw!';
+} else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+    return 'Draw!';
+} else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+    return 'You Lose! Rock beats Scissors';
+} else (playerSelection != 'rock' || 'paper' || 'scissors') 
+    return'You have made an invalid choice'
 }
 
-}
-
-const playerSelection = "Rock";
-const computerSelection = computerPlay()
-console.log(playRound(playerselection, computerSelection));
+const playerSelection = "rock";
+const computerSelection = computerPlay(random)
+console.log(playRound(playerSelection, computerSelection));
