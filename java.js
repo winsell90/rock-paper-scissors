000dfad4de286956374aaf164721c8df8e7fdf08
Warren Insell <insell.warren@gmail.com>
2022-04-07 11:27:14 -0700

    Add transition for selection and fix score display

--- java.js
+++ java.js
@@ -1,62 +1,76 @@
-function computerPlay(random) {
+function computerPlay() {
   return random[Math.floor(Math.random()*random.length)];
 }
 
 let random = ['rock', 'paper', 'scissors'];
 let playerScore = 0;
 let computerScore = 0;
+let ties = 0;
 let gamesPlayed = 0;
-
-let computerSelection = computerPlay(random)
 
 let results = document.querySelector('.results');
 let round = document.querySelector('.round');
+let humanScore = document.querySelector('.humanscore')
+let puterScore = document.querySelector('.puterscore')
 
 
 let rock = document.querySelector('.rock');
 rock.addEventListener('click', () => {
-playRound('rock', computerSelection)
+playRound('rock');
+rock.classList.add('clicked');
 });
+
+rock.addEventListener('transitionend', removeTransition);
+function removeTransition() {
+  this.classList.remove('clicked')
+}
 
 let paper = document.querySelector('.paper');
 paper.addEventListener('click', () => {
-  playRound('paper', computerSelection)
+  playRound('paper');
+  paper.classList.add('clicked');
 });
+
+paper.addEventListener('transitionend', removeTransition);
+function removeTransition() {
+  this.classList.remove('clicked')
+}
 
 let scissors = document.querySelector('.scissors');
 scissors.addEventListener('click', () => {
-  playRound('scissors', computerSelection)
-
+  playRound('scissors');
+scissors.classList.add('clicked');
 });
 
-
-function playRound(playerSelection, computerSelection) {
+scissors.addEventListener('transitionend', removeTransition);
+function removeTransition() {
+  this.classList.remove('clicked')
+}
+function playRound(playerSelection) {
+  let computerSelection = computerPlay();
   if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper')) {
     ++playerScore;
     ++gamesPlayed;
-    results.textContent = 'You win! The computer picked ' + computerSelection + `. 
-     Your score is ${playerScore} and the computer's score is ${computerScore}.`;
-     round.textContent = `The first to achieve a score of 5 wins the game. That was round ${gamesPlayed}.`;
+    results.textContent = 'You win! The computer picked ' + computerSelection + '. ' + `That was round ${gamesPlayed}.`;
+    humanScore.textContent = `Human: ${playerScore}`;
+    puterScore.textContent = `Computer: ${computerScore}`;
      victoryCondition();
 
   } else if ((playerSelection === 'rock' && computerSelection === 'paper') || (playerSelection === 'paper' && computerSelection === 'scissors') || (playerSelection === 'scissors' && computerSelection === 'rock')) {
     ++computerScore;
     ++gamesPlayed;
-    results.textContent = 'You lose! The computer picked ' + computerSelection + `. 
-     Your score is ${playerScore} and the computer's score is ${computerScore}.`;
-     round.textContent = `The first to achieve a score of 5 wins the game. That was round ${gamesPlayed}.`;
+    results.textContent = 'You lose! The computer picked ' + computerSelection + '. ' + `That was round ${gamesPlayed}.`;
+    humanScore.textContent = `Human: ${playerScore}`;
+    puterScore.textContent = `Computer: ${computerScore}`;
      victoryCondition();
      
 } else if ((playerSelection === 'rock' && computerSelection === 'rock') || (playerSelection === 'paper' && computerSelection === 'paper') || (playerSelection === 'scissors' && computerSelection === 'scissors')) {
-  results.textContent = 'It\'s a draw! The computer picked ' + computerSelection + `. 
-     Your score is ${playerScore} and the computer's score is ${computerScore}.`;
-     round.textContent = `The first to achieve a score of 5 wins the game. That was round ${gamesPlayed}.`;
-
-} else (playerSelection != 'rock' || 'paper' || 'scissors') 
-    return 'where are they clicking..?';
+  ++gamesPlayed;
+  ++ties;
+  results.textContent = 'It\'s a tie! The computer picked ' + computerSelection + `. 
+     That was tie round ${ties} and game round ${gamesPlayed}.`;
+} 
 }
-
-
 
 // below is the loop that plays 5 games for the original console version
 // for (; gamesPlayed < 5; ) {
@@ -68,9 +82,9 @@ function playRound(playerSelection, computerSelection) {
 
 function victoryCondition() {
   if (playerScore >= 5) {
-    alert(`You win the game! You won ${playerScore} times in ${gamesPlayed} rounds!`);
+    alert(`You win the game! You won ${playerScore} times in ${gamesPlayed} rounds! There were ${ties} ties. Click okay to refresh the page and begin a new game.`) ? '' : location.reload();
   } else if (computerScore >= 5) {
-    alert(`You lost the game! The computer won ${computerScore} times in ${gamesPlayed} rounds!`);
+    alert(`You lost the game! The computer won ${computerScore} times in ${gamesPlayed} rounds! There were ${ties} ties. Click okay to refresh the page and begin a new game.`) ? '' : location.reload();
     }
   }
 
